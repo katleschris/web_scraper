c4a4d05e9afc514ba8903604e8987284c975f3e1
Katlego Christabel Leshiba <katlegochristabelleshiba@gmail.com>
2024-06-25 11:42:42 +0200

    created parse search-results

--- webScraper.py
+++ webScraper.py
@@ -14,3 +14,17 @@ def fetch_page(url):
     except requests.RequestException as e:
         print(f"Failed to retrieve URL {url}: {e}")
         return None
+    
+
+def parse_search_results(html):
+    """
+    Parse the search results from the HTML to extract school URLs.
+    """
+    try:
+        soup = BeautifulSoup(html, 'html.parser')
+        search_results_div = soup.find("div", {"id": "search-results"})
+        media_body_divs = search_results_div.find_all("div", {"class": "media-body"})
+        return [a_tag.find("a", href=True)["href"].strip() for a_tag in media_body_divs if a_tag.find("a", href=True)]
+    except AttributeError as e:
+        print(f"Error parsing search results HTML: {e}")
+        return []
